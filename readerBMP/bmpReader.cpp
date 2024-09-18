8f4ac048b727bc8886f8f305c2a4648d96c3f29f
Denis Petrov <mebelholm@yandex.ru>
2024-09-18 13:17:18 -0300

    edit readerBMP for win and linux

--- readerBMP/bmpReader.cpp
+++ readerBMP/bmpReader.cpp
@@ -1,8 +1,36 @@
 ﻿// Viewer b\w BMP files for Windows, test task Petrov Denis 2024
 #include <iostream>
-#include <windows.h>
 #include <fstream>
 #include <vector>
+
+#ifdef _WIN32
+    #include <windows.h> // Для Windows
+#else
+    // Определения структур для Linux
+    #pragma pack(push, 1)
+    struct BITMAPFILEHEADER {
+        uint16_t bfType;
+        uint32_t bfSize;
+        uint16_t bfReserved1;
+        uint16_t bfReserved2;
+        uint32_t bfOffBits;
+    };
+
+    struct BITMAPINFOHEADER {
+        uint32_t biSize;
+        int32_t biWidth;
+        int32_t biHeight;
+        uint16_t biPlanes;
+        uint16_t biBitCount;
+        uint32_t biCompression;
+        uint32_t biSizeImage;
+        int32_t biXPelsPerMeter;
+        int32_t biYPelsPerMeter;
+        uint32_t biClrUsed;
+        uint32_t biClrImportant;
+    };
+    #pragma pack(pop)
+#endif
 
 class ReaderBMP {
     std::string fileName_;
